bd167c399c6831909203d050fcae49aec1d8b006
lilalr <gurennut@gmail.com>
2026-01-02 18:26:59 +0700

    menambahkan implementasi FirebaseRepositorySiswa menggunakan Firebase Firestore

--- app/src/main/java/com/example/questfirebase_041/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_041/repositori/RepositorySiswa.kt
@@ -9,3 +9,7 @@ interface RepositorySiswa {
     suspend fun postDataSiswa(siswa: Siswa)
 }
 
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+
