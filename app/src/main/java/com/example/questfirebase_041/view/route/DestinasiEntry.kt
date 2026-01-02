e96ff0033cdd61266cbc1430de175ad9adbaa3e7
lilalr <gurennut@gmail.com>
2026-01-02 18:32:48 +0700

    menambahkan DestinasiEntry untuk navigasi ke halaman tambah data siswa

---
+++ app/src/main/java/com/example/questfirebase_041/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_041.view.route
+
+import com.example.questfirebase_041.view.route.DestinasiNavigasi
+import com.example.questfirebase_041.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
