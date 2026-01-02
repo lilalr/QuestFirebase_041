cbbae269700e5d32b0c04d0d5f60d466cc82b990
lilalr <gurennut@gmail.com>
2026-01-02 18:41:55 +0700

    menambahkan HomeViewModel untuk memuat dan mengelola data siswa

--- app/src/main/java/com/example/questfirebase_041/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questfirebase_041/viewmodel/HomeViewModel.kt
@@ -16,3 +16,24 @@ sealed interface StatusUiSiswa {
     object Loading : StatusUiSiswa
 }
 
+class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: Exception) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
