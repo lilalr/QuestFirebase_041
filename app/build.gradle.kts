68fbdc8b21285b57a53b38a13e57683658c24776
lilalr <gurennut@gmail.com>
2026-01-02 18:08:46 +0700

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
 
     //Firebase
     implementation(platform(libs.firebase.bom))
@@ -61,6 +62,7 @@ dependencies {
     //Viewmodel
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
