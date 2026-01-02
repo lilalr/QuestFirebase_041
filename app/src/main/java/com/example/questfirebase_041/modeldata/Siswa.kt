836fc6bf0c5e15ed72264d38c117ec9ebb6186ed
lilalr <gurennut@gmail.com>
2026-01-02 18:11:40 +0700

    menambahkan fungsi ekstensi toDetailSiswa() untuk konversi dari Siswa ke DetailSiswa

--- app/src/main/java/com/example/questfirebase_041/modeldata/Siswa.kt
+++ app/src/main/java/com/example/questfirebase_041/modeldata/Siswa.kt
@@ -15,3 +15,5 @@ data class DetailSiswa(
 )
 
 fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
+
