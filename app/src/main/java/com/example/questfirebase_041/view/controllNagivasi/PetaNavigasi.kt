3187fdd16da0bc8a1686fcc44c9dc4cde354c5d5
lilalr <gurennut@gmail.com>
2026-01-09 18:29:07 +0700

    menambah composable edit siswa screen

--- app/src/main/java/com/example/questfirebase_041/view/controllNagivasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_041/view/controllNagivasi/PetaNavigasi.kt
@@ -51,6 +51,11 @@ fun HostNavigasi(
                 .route}/$it")},
                 navigateBack = { navController.navigate(DestinasiHome.route) })
         }
-
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiEdit.itemIdArg)
+        { type= NavType.StringType})){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
+        }
     }
 }
