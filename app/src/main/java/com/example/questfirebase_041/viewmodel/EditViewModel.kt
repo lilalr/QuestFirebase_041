40fb56d5c4012c60a6627416a4109b1599108050
lilalr <gurennut@gmail.com>
2026-01-09 18:35:08 +0700

    membuat proses update data siswa ke repository

--- app/src/main/java/com/example/questfirebase_041/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_041/viewmodel/EditViewModel.kt
@@ -43,5 +43,15 @@ RepositorySiswa
         }
     }
 
-
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa
+                    ())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
 }
