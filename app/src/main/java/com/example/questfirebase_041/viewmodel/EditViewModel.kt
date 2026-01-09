df5ab09a9a1c6ac3ea51bc6cbdcada0f67a438b7
lilalr <gurennut@gmail.com>
2026-01-09 18:34:35 +0700

    membuat validasi input form (tidak boleh kosong)

--- app/src/main/java/com/example/questfirebase_041/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_041/viewmodel/EditViewModel.kt
@@ -37,4 +37,11 @@ RepositorySiswa
                 (detailSiswa))
     }
 
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+
 }
