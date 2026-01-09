ba733bc02db8bdbb2008c7329be4dd26d3be589d
lilalr <gurennut@gmail.com>
2026-01-09 18:27:33 +0700

    menambah fungsi CRUD detail, edit, dan hapus siswa

--- app/src/main/java/com/example/questfirebase_041/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_041/repositori/RepositorySiswa.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.tasks.await
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+    suspend fun getSatuSiswa(id: Long): Siswa?
+    suspend fun editSatuSiswa(id: Long, siswa: Siswa)
+    suspend fun hapusSatuSiswa(id: Long)
 }
 
 class FirebaseRepositorySiswa : RepositorySiswa {
@@ -38,4 +41,5 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         )
         docRef.set(data).await()
     }
+
 }
